a77984ffd2d7f7283c85296d2a5ab4a8d5eaf3fa
Arin <71049312+arin808@users.noreply.github.com>
2023-02-09 15:30:59 -0700

    Backend connection to mongo
    Local server app to connect to mongo is established

--- server/app.js
+++ server/app.js
@@ -1,14 +1,29 @@
-const express = require('express');
+//Description: This is the main file of the server
+
+//Import modules/dependencies
+const { mongoConnect } = require("./config");
+const PORT = process.env.PORT;
+const express = require("express");
 const app = express();
+const bodyParse = require("body-parser");
+const cors = require("cors");
 
-require('dotenv/config');
-const api = process.env.API_URL;
+//Function to initiate server connection to MongoDB
+async function startMongoServer() {
+  await mongoConnect();
 
-//api
-app.get('/', (req, res) => {
-    res.send('hello world');
-})
+  //Utilize parser for requests
+  app.use(bodyParse.urlencoded({ extended: false }));
+  app.use(bodyParse.json());
+  app.use(cors());
 
-app.listen(`${api}/instruments`, ()=>{
-    console.log('server is running on localhost:3000');
-})
+  //API route connections
+
+  //Listen for active app on Port 3000
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
+}
+
+//Start server
+startMongoServer();
